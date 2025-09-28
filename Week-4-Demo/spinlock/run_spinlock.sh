4827ec4ba80c3c0fd5f5d89d858beb994ef3fc62
Sidharth Sundar <sidharth.sundar@live.com>
2025-09-28 19:48:51 +0200

    updated spinlock testing script

--- Week-4-Demo/spinlock/run_spinlock.sh
+++ Week-4-Demo/spinlock/run_spinlock.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
+
+OUTPUT_LINES=16
+RESULTS_DIR=results
+
+ops_per_thread=1000000
+num_readers=5
+ 
+if [ ! -z "$1" ]; then
+    ops_per_thread=$1
+fi
+ 
+if [ ! -z "$2" ]; then
+    num_readers=$2
+fi
+ 
+mkdir ./${RESULTS_DIR}
+
 make
 sudo insmod sync_spinlock.ko
 sleep 5
 sudo rmmod sync_spinlock
-sudo dmesg | grep -i SYNC_SPINLOCK | tail -n 30
+sudo dmesg | grep -i SYNC_SPINLOCK | tail -n ${OUTPUT_LINES} > ./${RESULTS_DIR}/spinlock_ops-${ops_per_thread}_readers-${num_readers}.out
+make clean
