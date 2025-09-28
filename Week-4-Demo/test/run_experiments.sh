4140a84ef67e60de9c2e67e8b3ccbe9a821457d8
Sidharth Sundar <sidharth.sundar@live.com>
2025-09-28 20:19:41 +0200

    full experiment script

---
+++ Week-4-Demo/test/run_experiments.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+BASE_DIR=$(realpath ..);
+
+SYNC_DIRS=(spinlock rwspinlock seqlock rcu)
+READER_COUNTS=(1 5 9)
+OPS=(1000000)
+
+for dirname in ${SYNC_DIRS[@]}; do
+    cd "${BASE_DIR}/${dirname}"
+    for ops in ${OPS[@]}; do
+	for readers in ${READER_COUNTS[@]}; do
+	    bash run_${dirname}.sh ${ops} ${readers}
+	done	
+    done
+done
+
+
